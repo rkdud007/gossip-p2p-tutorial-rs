903e9bbb0070df5648a27e9a36c3b39914118403
Pia <gayeongparkk@gmail.com>
2024-01-27 22:02:31 +0800

    Transport, NetworkBehaviour, Swarm,Idle connection timeout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use libp2p::ping;
+use std::{error::Error, time::Duration};
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
@@ -7,7 +8,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity();
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_async_std()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::tls::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30))) // Allows us to observe pings for 30 seconds.
+        .build();
 
     Ok(())
 }
