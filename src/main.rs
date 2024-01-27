616232cfb443c7261088cb39deccb53a7a95b41e
Pia <gayeongparkk@gmail.com>
2024-01-27 21:58:30 +0800

    Network identity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity();
+
+    Ok(())
 }
